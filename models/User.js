677a81d93312c886e1747caf77dc88725430f19a
lucas <lucas.salvino@ccc.ufcg.edu.br>
2018-09-12 15:42:40 -0300

    login funcionando parcialmente junto com o bd

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+var bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
 	email:{
 		type:String,
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 const User = require('./models/User');
 const userSession = require('./models/UserSession');
+mongoose.connect('mongodb://localhost:27017/myapp');
+
 app.get('/api/customers', (req, res) => {
   const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
@@ -14,61 +23,40 @@ app.get('/api/customers', (req, res) => {
 });
 
 app.post('/api/signin', (req, res) => {
-  User.find({
-  	email:email
-  },(err,users)=>{
-  	if(err){
-  		res.send({erro:'erro na tentativa de login',
-  					login:'false'});
-  	}
-
-  	const user = users[0];
-  	if(!user.validPassword(password)){
-  		res.send('password incorreto!')
-  	}
-
-  	const userSession = new UserSession();
-  	userSession.userId = user._id;
-  	userSession.save((err, doc)=>{
-  		if(err){
-  			res.send('erro no servidor');
-  		}
-
-  		res.send('Login Realizado com sucesso');
-  	});
-  });
+  User.findOne({email:req.body.email}, (err, users)=>{
+		if(err){
+			res.send({email:'serverError', password:'servererror'});
+		}else{
+			res.send(users);
+		}
+	});
 });
 
 app.post('/api/signup', (req, res, next) => {
-	const{ body } = req;
-	const{
-		email,
-		password
-	} = body;
+	
 
-	email=email.toLowerCase();
-  User.find({
-  	email:email
-  },(err, prevUser)=>{
-  	if(err){
-  		res.end('erro no servidor')
-  	}else if(prevUser>0){
-  		res.end('Conta ja existente')
-  	}
+			email=req.body.email.toLowerCase();
+	  	const newUser = new User();
 
-  	const newUser = new User();
+	  	newUser.email = email;
+	  	newUser.password = req.body.password;
+	  	newUser.save((err, user)=>{
+	  		if(err){
+	  			res.send({email:'erro', password:'erro'});
+	  		}else{
+	  			res.send({email:'sucesso', password:'sucesso'});
+	  		}
+	  	});
+	});
 
-  	newUser.email = email;
-  	newUser.password = newUser.generateHash(password);
-  	newUser.save((err, user)=>{
-  		if(err){
-  			res.end('erro no servidor');
-  		}else{
-  			res.end('cadastro realizado com sucesso');
-  		}
-  	});
-  });
-});
+	
+
+  	
+
+  	
+
+
+
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
